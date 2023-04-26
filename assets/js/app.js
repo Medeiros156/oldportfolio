38c52cd1033df8eb2ccc0ac5d47814440165f7d9
Pedro Medeiros <pedromedeiros156@hotmail.com>
2023-04-26 02:33:51 -0300

    responsive3

--- assets/js/app.js
+++ assets/js/app.js
@@ -60,34 +60,40 @@ let d1 = circle1.getAttribute("d");
 let d2 = circle2.getAttribute("d");
 let d3 = circle3.getAttribute("d");
 
-if (window.innerWidth < 768) {
-  circle1.setAttribute(
-    "d",
-    "M 920 920 m -320 0 a 320 320 0 1 0 640 0 a 320 320 0 1 0 -640 0"
-  );
-  circle2.setAttribute(
-    "d",
-    "M 920 920 m -400 0 a 400 400 0 1 0 800 0 a 400 400 0 1 0 -800 0"
-  );
-  circle3.setAttribute(
-    "d",
-    "M 920 920 m -480 0 a 480 480 0 1 0 960 0 a 480 480 0 1 0 -960 0"
-  );
-} else if (window.innerWidth < 1081) {
-  circle1.setAttribute(
-    "d",
-    "M 920 920 m -320 0 a 320 320 0 1 0 640 0 a 320 320 0 1 0 -640 0"
-  );
-  circle2.setAttribute(
-    "d",
-    "M 920 920 m -420 0 a 420 420 0 1 0 840 0 a 420 420 0 1 0 -840 0"
-  );
-  circle3.setAttribute(
-    "d",
-    "M 920 920 m -520 0 a 520 520 0 1 0 1040 0 a 520 520 0 1 0 -1040 0"
-  );
-} else {
-  circle1.setAttribute("d", d1);
-  circle2.setAttribute("d", d2);
-  circle3.setAttribute("d", d3);
+function resizeSVG() {
+  if (window.innerWidth < 768) {
+    circle1.setAttribute(
+      "d",
+      "M 920 920 m -320 0 a 320 320 0 1 0 640 0 a 320 320 0 1 0 -640 0"
+    );
+    circle2.setAttribute(
+      "d",
+      "M 920 920 m -400 0 a 400 400 0 1 0 800 0 a 400 400 0 1 0 -800 0"
+    );
+    circle3.setAttribute(
+      "d",
+      "M 920 920 m -480 0 a 480 480 0 1 0 960 0 a 480 480 0 1 0 -960 0"
+    );
+  } else if (window.innerWidth < 1081) {
+    circle1.setAttribute(
+      "d",
+      "M 920 920 m -320 0 a 320 320 0 1 0 640 0 a 320 320 0 1 0 -640 0"
+    );
+    circle2.setAttribute(
+      "d",
+      "M 920 920 m -420 0 a 420 420 0 1 0 840 0 a 420 420 0 1 0 -840 0"
+    );
+    circle3.setAttribute(
+      "d",
+      "M 920 920 m -520 0 a 520 520 0 1 0 1040 0 a 520 520 0 1 0 -1040 0"
+    );
+  } else {
+    circle1.setAttribute("d", d1);
+    circle2.setAttribute("d", d2);
+    circle3.setAttribute("d", d3);
+  }
 }
+
+resizeSVG();
+
+window.addEventListener("resize", resizeSVG);
--- assets/js/matrixBackground.js
+++ assets/js/matrixBackground.js
@@ -1,28 +1,57 @@
 const canvas = document.getElementById("Matrix");
 const context = canvas.getContext("2d");
 
-const nums = "0123456789";
-const latin = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const asia =
-  "アァカサタナハシチニヒミリヰギジヂビピウゥクスツヌフテネヘメレヱゲゼデベペロヲゴゾドボポヴッン";
+// const nums = "0123456789";
+// const latin = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// const asia =
+//   "アァカサタナハシチニヒミリヰギジヂビピウゥクスツヌフテネヘメレヱゲゼデベペロヲゴゾドボポヴッン";
 
-const letters = asia + latin + nums;
+// const letters = asia + latin + nums;
+const letters = "01";
 
 const fontSize = 14;
 
 let columns = 0;
 let rainDrops = [];
 
+// if (window.innerWidth < 1080) {
+//   // Tablet
+//   canvas.width = window.innerWidth - 10;
+//   // canvas.height = document.body.clientHeight - 10;
+//   canvas.height = document.documentElement.scrollHeight;
+//   console.log("docdoc", document.documentElement.scrollHeight);
+//   console.log("canvas", canvas.height);
+// } else if (window.innerWidth < 768) {
+//   // Mobile
+//   canvas.width = window.innerWidth - 10;
+//   canvas.height = document.body.clientHeight - 35;
+//   console.log("doc", document.body.clientHeight);
+//   console.log("canvas", canvas.height);
+// } else {
+//   // Desktop
+//   canvas.height = document.body.clientHeight - 50;
+//   canvas.width = window.innerWidth - 10;
+//   console.log("doc", document.body.clientHeight);
+//   console.log("canvas", canvas.height);
+// }
 const resizeCanvas = () => {
   if (window.innerWidth < 768) {
-    // Mobile
     canvas.width = window.innerWidth - 10;
-    canvas.height = document.body.clientHeight - 250;
+    canvas.height = document.body.scrollHeight;
+    console.log("docdoc1", document.body.scrollHeight);
+    console.log("canvas1", canvas.height);
+  } else if (window.innerWidth < 1080) {
+    canvas.width = window.innerWidth - 10;
+    canvas.height = document.body.scrollHeight - 20;
+    console.log("docdoc2", document.body.scrollHeight);
+    console.log("canvas2", canvas.height);
   } else {
     canvas.width = window.innerWidth - 10;
-    canvas.height = document.body.clientHeight - 50;
-    // Desktop
+    canvas.height = document.body.scrollHeight - 50;
+    console.log("docdoc3", document.body.scrollHeight);
+    console.log("canvas3", canvas.height);
   }
+
   columns = canvas.width / fontSize;
   console.log(columns);
   rainDrops = [];
@@ -36,8 +65,12 @@ const draw = () => {
   context.fillRect(0, 0, canvas.width, canvas.height);
   // console.log(canvas.height);
   // console.log(canvas.width);
-
-  context.fillStyle = "#0F0";
+  if (Math.random() > 0.5) {
+    context.fillStyle = "#ff3366";
+  } else {
+    context.fillStyle = "#25724b";
+  }
+  // context.fillStyle = "linear-gradient(blue,red)";
   context.font = fontSize + "px monospace";
 
   for (let i = 0; i < rainDrops.length; i++) {
